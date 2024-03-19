6a9a5ee676e2cf238b2a93ea74559f90ec1402ed
huyet120 <163976645+huyet120@users.noreply.github.com>
2024-03-19 14:03:55 +0700

    Add files via upload

---
+++ SoChinhPhuong.cpp
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+// Hàm kiểm tra số chính phương
+int isPerfectSquare(int num) {
+	int squareRoot = (int)sqrt(num);
+	return (squareRoot * squareRoot == num);
+}
+
+// Hàm đếm và in ra các số chính phương nhỏ hơn n
+void countAndPrintPerfectSquares(int n) {
+	printf("Cac so chinh phuong nho hon %d:\n", n);
+
+	for (int i = 1; i < n; i++) {
+		if (isPerfectSquare(i)) {
+			printf("%d ", i);
+		}
+	}
+
+	printf("\n");
+}
+
+int main() {
+	int n;
+
+	printf("Nhap so nguyen duong n: ");
+	scanf("%d", &n);
+
+	countAndPrintPerfectSquares(n);
+
+	return 0;
+}
